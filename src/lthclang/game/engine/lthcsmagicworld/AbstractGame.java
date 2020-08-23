19f72c8c16f0a55d68fead36ebbbe69a0af2a87a
LTHCTheMaster <65359406+LTHCTheMaster@users.noreply.github.com>
2020-08-23 14:12:37 +0200

    Delete AbstractGame.java

--- src/lthclang/game/engine/lthcsmagicworld/AbstractGame.java
+++
@@ -1,7 +0,0 @@
-package lthclang.game.engine.lthcsmagicworld;
-
-public abstract class AbstractGame 
-{
-	public abstract void update(GameContainer gc, float dt);
-	public abstract void render(GameContainer gc, Renderer r);
-}
